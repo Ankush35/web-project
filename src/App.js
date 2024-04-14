e03b737b4d06489a643ed7a13e90ae7dd485d199
Divyanshu Singh <singhdivyanshu2707@gmail.com>
2024-04-14 15:07:46 +0530

    feat: setup routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import About from "./routes/About";
+import Wishlist from "./routes/Wishlist";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Router>
+			<Routes>
+				<Route path="/about" element={<About />}></Route>
+				<Route path="/wishlist" element={<Wishlist />}></Route>
+			</Routes>
+		</Router>
+	);
 }
 
 export default App;
